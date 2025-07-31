5e92d542f9adf76fa5f34bc8f9c4a21eef223e3e
RAJARAJAN R <iamrajarajanr@gmail.com>
2025-07-31 14:55:49 +0530

    class end

--- index.js
+++ index.js
@@ -20,7 +20,10 @@ renderLeads();
 function renderLeads(){
 let listItems = "";
 for (let i = 0; i < myLeads.length; i++){
-   listItems += "<li>"+myLeads[i]+"</li>"
+   listItems += `<li><a target="_blank" href ="${myLeads[i]}">${myLeads[i]}</a></li>`
+
+
+
 
 }
 ulEl.innerHTML = listItems;
