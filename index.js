bec79f11465a3a3da7d3d39802c7a9b9d5c1a8de
RAJARAJAN R <iamrajarajanr@gmail.com>
2025-07-31 12:14:25 +0530

    ch

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ let inputBtn = document.getElementById("input-btn")
 let inputEl = document.getElementById("input-el")
 let ulEl = document.getElementById("ul-el")
 let myLeads = ["apple","orange"]
+let con = document.getElementById("container")
 
 // inputBtn.addEventListener("click", function(){
 //     console.log("btn clicked")
@@ -18,7 +19,12 @@ ulEl.textContent += myLeads[i]+" ";
 
 for (let i = 0; i < myLeads.length; i++){
     console.log(myLeads[i]);
+    ulEl.innerHTML += "<li" + myLeads[i]+"</li>";
 
 }
+
+con.innerHTML += "<button>Click me</button>"
+
+
 
 
