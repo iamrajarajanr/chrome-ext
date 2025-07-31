eeb91df10ec289586e524445464149b2fd24def7
RAJARAJAN R <iamrajarajanr@gmail.com>
2025-07-31 10:35:56 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+let inputBtn = document.getElementById("input-btn")
+
+inputBtn.addEventListener("click", function(){
+    console.log("btn clicked")
+})
+
+// inputBtn.addEventListener("click", print)
+// function print(){
+//     console.log("printed")
+// }
