bfdfd838b8581ea25423ccf7b4143b3ccfc45611
RAJARAJAN R <iamrajarajanr@gmail.com>
2025-08-01 12:28:22 +0530

    ch

--- basics.js
+++ basics.js
@@ -1,27 +1,67 @@
-let inputBtn = document.getElementById("input-btn")
+// let inputBtn = document.getElementById("input-btn")
 
-inputBtn.addEventListener("click", function(){
-    console.log("btn clicked")
-})
+// inputBtn.addEventListener("click", function(){
+//     console.log("btn clicked")
+// })
 
-// or
+// // or
 
-inputBtn.addEventListener("click", print)
-function print(){
-    console.log("printed")
-}
+// inputBtn.addEventListener("click", print)
+// function print(){
+//     console.log("printed")
+// }
 
-// "Let" can define mulitple times
-let user = "admin"
-user = "user alone"
-user = "changed"
-console.log(user)
+// // "Let" can define mulitple times
+// let user = "admin"
+// user = "user alone"
+// user = "changed"
+// console.log(user)
 
-// "const" can define only once times
-const player = "john"
-const array = ["apple","orange",]
-array.push("mango")
-console.log(array)
+// // "const" can define only once times
+// const player = "john"
+// const array = ["apple","orange",]
+// array.push("mango")
+// console.log(array)
+
+
+// // innerHTML
+// const btnText = "click me"
+// con.innerHTML += "<button id='butn' >"+btnText+"</button>"
+
+// document.getElementById("butn").addEventListener("click", function(){
+//     con.innerHTML += "<img style='display:block' src='./icon.png' >"
+// })
+
+
+// //localStorage
+// let fruits = `["apple","orange"]`
+// fruits.push("cake")
+// localStorage.setItem("fruitsArr", JSON.stringify(fruits))
+// const fromLocal = JSON.parse(localStorage.getItem("fruitsArr"))
+// console.log(fromLocal)
+
+// // localstorage set and remove
+// const local = localStorage.getItem("myName")
+// localStorage.setItem ("myName", ["wow","apple",123]);
+// localStorage.setItem("user",123)
+// localStorage.setItem("product","iphone")
+// localStorage.removeItem("user")
+// localStorage.clear()
+
+// console.log(local);
+// console.log(typeof local)
 
 
 
+// //falsy and truthy
+// console.log(Boolean(undefined))
+// console.log(Boolean(""))
+// console.log(Boolean(null))
+// console.log(Boolean(0))
+// console.log(Boolean(false))
+// console.log(Boolean("false"))
+// console.log(Boolean("hi"))
+// console.log(Boolean([]))
+// console.log(Boolean({}))
+// console.log(Boolean(["hello",123]))
+// console.log(Boolean(NaN))
--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 let inputBtn = document.getElementById("input-btn")
 let inputEl = document.getElementById("input-el")
 let ulEl = document.getElementById("ul-el")
-let myLeads = ["apple","orange"]
+let myLeads = []
 let con = document.getElementById("container")
 
 // inputBtn.addEventListener("click", function(){
 //     console.log("btn clicked")
 // })
 
-const fromLocal = JSON.parse(localStorage.getItem("myLeads"))
-console.log(fromLocal)
+let leadfromLocal = JSON.parse(localStorage.getItem("myLeads"))
+console.log(leadfromLocal)
+
+if (leadfromLocal){
+   myLeads = leadfromLocal
+   renderLeads()
+}else{
+   ulEl.textContent = "No data to show, Add something in input and click save input button"
+}
 
 
 inputBtn.addEventListener("click", function(){
@@ -20,6 +27,7 @@ localStorage.setItem("myLeads", JSON.stringify(myLeads))
 inputEl.value = " "
 renderLeads();
 });
+
 
 function renderLeads(){
 let listItems = "";
@@ -33,63 +41,3 @@ for (let i = 0; i < myLeads.length; i++){
 ulEl.innerHTML = listItems;
 
 }
-
-// const local = localStorage.getItem("myName")
-
-// localStorage.setItem ("myName", ["wow","apple",123]);
-// localStorage.setItem("user",123)
-// localStorage.setItem("product","iphone")
-
-// localStorage.removeItem("user")
-
-// // localStorage.clear()
-
-
-// console.log(local);
-// // console.log(typeof local)
-
-
-// // const btnText = "click me"
-// // con.innerHTML += "<button id='butn' >"+btnText+"</button>"
-
-
-// // document.getElementById("butn").addEventListener("click", function(){
-// //     con.innerHTML += "<img style='display:block' src='./icon.png' >"
-// // })
-
-
-// let fruits = `["apple","orange"]`
-// fruits = JSON.parse(fruits)
-// fruits.push("cake")
-
-// localStorage.setItem("fruitsArr", JSON.stringify(fruits))
-
-// const fromLocal = JSON.parse(localStorage.getItem("fruitsArr"))
-// console.log(fromLocal)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
