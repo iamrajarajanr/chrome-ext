1d365017788657342658a096b2da01da74ca384d
RAJARAJAN R <iamrajarajanr@gmail.com>
2025-08-01 11:36:39 +0530

    ch

--- index.js
+++ index.js
@@ -8,11 +8,15 @@ let con = document.getElementById("container")
 //     console.log("btn clicked")
 // })
 
-
+const fromLocal = JSON.parse(localStorage.getItem("myLeads"))
+console.log(fromLocal)
 
 
 inputBtn.addEventListener("click", function(){
 myLeads.push(inputEl.value)
+
+localStorage.setItem("myLeads", JSON.stringify(myLeads))
+
 inputEl.value = " "
 renderLeads();
 });
@@ -30,13 +34,38 @@ ulEl.innerHTML = listItems;
 
 }
 
-// const btnText = "click me"
-// con.innerHTML += "<button id='butn' >"+btnText+"</button>"
+// const local = localStorage.getItem("myName")
+
+// localStorage.setItem ("myName", ["wow","apple",123]);
+// localStorage.setItem("user",123)
+// localStorage.setItem("product","iphone")
+
+// localStorage.removeItem("user")
+
+// // localStorage.clear()
 
 
-// document.getElementById("butn").addEventListener("click", function(){
-//     con.innerHTML += "<img style='display:block' src='./icon.png' >"
-// })
+// console.log(local);
+// // console.log(typeof local)
+
+
+// // const btnText = "click me"
+// // con.innerHTML += "<button id='butn' >"+btnText+"</button>"
+
+
+// // document.getElementById("butn").addEventListener("click", function(){
+// //     con.innerHTML += "<img style='display:block' src='./icon.png' >"
+// // })
+
+
+// let fruits = `["apple","orange"]`
+// fruits = JSON.parse(fruits)
+// fruits.push("cake")
+
+// localStorage.setItem("fruitsArr", JSON.stringify(fruits))
+
+// const fromLocal = JSON.parse(localStorage.getItem("fruitsArr"))
+// console.log(fromLocal)
 
 
 
